2c14b628d1ae9449a89298d51568f235cc348fd7
Felipe Pineda <fpineda@live.unc.edu>
2020-07-21 16:48:10 -0400

    Implemented async-await into API endpoints

--- src/index.js
+++ src/index.js
@@ -12,60 +12,66 @@ app.use(express.json())
 
 // POST: localhost:3000/users
 // DESCRIPTION: Creates new user (name, email, password)
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    // saves the document
-    user.save().then(() =>{
-        res.status.apply(201).send(user) // sent to the sender
-    }).catch((error) => {
-        res.status(400).send(error) // bad request.error
-    })
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
 // GET: localhost:3000/users
 // DESCRIPTION: Obtain a list of users
 // This endpoint uses Mongoose API
 // Mongoose Queries: Model.find() where Model is User (see requires)
-app.get('/users', (req, res) => {
-    // fetch all user in the database, we can access to those via the promise. Promise is fulfilled with the users, or be rejected with an error
-    User.find({}).then((users) =>{
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send() // internal service error.send nothing as it already send stuff
-    })  
+    } 
 })
 
 // GET: localhost:3000/users/:id (localhost:3000/users/123456789)
 // DESCRIPTION: Obtain a user from a given id
 // This endpoint uses Mongoose API
 // Mongoose Queries: Model.findOne() where Model is User (see requires)
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id // params is an object with key value pairs created when you call the endpoint with params in the URL
     
-    // find user
-    User.find({_id}).then((user) =>{
+    try {
+        const user = await User.find({_id})
         if (!user){
             return res.status(404).send()
         }
-        res.send(user) // will not be changing the status code
-    }).catch((error) => {
+        res.send(user)
+    } catch (error) {
         res.sendStatus(500).send()
-    })  
+    } 
 })
+
+
+
+
+
+
 
 
 // POST: localhost:3000/tasks
 // DESCRIPTION: Creates new task (description, complete)
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    // saves the document
-    task.save().then(() => {
+    try{
+        await task.save()
         res.status(201).send(task) // send to the sender
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error) // bad request.error
-    })
+    }
 })
 
 
@@ -73,31 +79,31 @@ app.post('/tasks', (req, res) => {
 // DESCRIPTION: Obtain a list of tasks [ for specific user ]
 // This endpoint uses Mongoose API
 // Mongoose Queries: Model.find() where Model is Task (see requires)
-app.get('/tasks', (req, res) => {
-    // fetch all user in the database, we can access to those via the promise. Promise is fulfilled with the users, or be rejected with an error
-    Task.find({}).then((tasks) =>{
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send() // internal service error.send nothing as it already send stuff
-    })  
+    } 
 })
 
 // GET: localhost:3000/tasks/:id (localhost:3000/tasks/123456789) 
 // DESCRIPTION: Obtain a user from a given id [ for specific user ]
 // This endpoint uses Mongoose API
 // Mongoose Queries: Model.findOne() where Model is Task (see requires)
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id // params is an object with key value pairs created when you call the endpoint with params in the URL
     
-    // find task
-    Task.find({_id}).then((task) =>{
+    try {
+        const task = await Task.find({_id})
         if (!task){
             return res.status(404).send()
         }
-        res.send(task) // will not be changing the status code
-    }).catch((error) => {
+        res.send(task)
+    } catch (error) {
         res.sendStatus(500).send()
-    })  
+    } 
 })
 
 
