c849ab42c38a5967ec60698ebd9ba637933505ae
Felipe Pineda <fpineda@live.unc.edu>
2020-07-21 15:19:30 -0400

    Using async-await with async/promise/mongoose functions

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,12 +3,24 @@ const Task = require('../src/models/task')
 
 // Promise chaining in Mongoose API
 
-// Two async operations: remove a given task by id and then get and print the total number of incomplete tasks
-Task.findByIdAndDelete('5f108a2dd7d108460d767afe').then((task) => {
-    console.log(task)
-    return Task.countDocuments({complete: false})
-}).then((result) => {
-    console.log(result)
+const deleteTaskAndCount = async (id, complete) => {
+    const deletedTask = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ complete: complete })
+    return count
+}
+
+deleteTaskAndCount('5f108a60d7d108460d767b00', false).then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
+
+// Two async operations: remove a given task by id and then get and print the total number of incomplete tasks
+// Task.findByIdAndDelete('5f108a2dd7d108460d767afe').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({complete: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,24 +3,25 @@ const User = require('../src/models/user')
 
 // Promise chaining in Mongoose API
 
-// Template
-///////////////////////////////
-// add(1,1).then((sum) => {
-//     console.log(sum)
-//     return add(sum, 4)
-// }).then((sum2) => {
-//     console.log(sum2)
-// }).catch((error) => {
-//     console.log(error)
-// })
-//////////////////////////////
+// creating an async function to await Mongoose API
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age })
+    const count = await User.countDocuments({ age: age })
+    return count
+}
 
-// two async operations: update user's age and then count # of users with age 1
-User.findByIdAndUpdate('5f0deab3fb6d001ddb8246e6', {age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age: 1})
-}).then((result) => {
-    console.log(result)
+updateAgeAndCount('5f0deab3fb6d001ddb8246e6', 2).then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
+
+// two async operations: update user's age and then count # of users with age 1
+// User.findByIdAndUpdate('5f0deab3fb6d001ddb8246e6', {age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
