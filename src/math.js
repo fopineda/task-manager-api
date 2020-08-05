1050ce2e63e28cf0445b21c43a2300cf810404a7
Felipe Pineda <fpineda@live.unc.edu>
2020-08-05 10:58:08 -0400

    added async tests to test dummy async function

--- src/math.js
+++ src/math.js
@@ -11,8 +11,22 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+// promise function
+const add = (a,b) => {
+    // Create the promise with some task
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0){
+                return reject('Number must be non-negative')
+            }
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
