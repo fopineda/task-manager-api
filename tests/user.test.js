3ed4921824721de2928c9682c37d4d8befb8ffd0
Felipe Pineda <fpineda@live.unc.edu>
2020-08-06 11:01:27 -0400

    testing endpoints (get user profile, delete user account) with authentication

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
-const userOne = { 
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@example.com',
-    password: 'MyPass777!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 // lifecycle methods (ex: beforeEach, afterEach, beforeAll, afterAll, etc.)
@@ -39,4 +46,41 @@ test('Should not login nonexisting user', async () => {
         email: 'bad-email',
         password: 'bad-password'
     }).expect(400)
+})
+
+
+// GET: get user profile
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+// GET: get user profile
+test('Should not get profile for user for unauthenticated user', async () => {
+    // send without authentication token
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+// DELETE: delete user account
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+// DELETE: delete user account
+test('Should not delete account for unauthenticated user', async () => {
+    // send without authentication token. Also, expecting 401 as not authenticated even though route says 500 is sent
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
 })
